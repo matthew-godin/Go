60e58347b91d21cf97bfb62a38497aa62685aab2
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-03-04 08:59:11 -0500

    channel select statement

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"time"
 )
 
 /*import (
@@ -190,19 +190,35 @@ func main() {
 	if err != nil {
 		fmt.Println(fmt.Errorf("invalid input: %w", err))
 	}*/
-	var wg sync.WaitGroup
+	/*var wg sync.WaitGroup
 	ch := make(chan int)
 	wg.Add(1)
 	go func() {
 		ch <- 42
-		/*fmt.Println("go routine")
-		wg.Done()*/
+		//fmt.Println("go routine")
+		//wg.Done()
 	}()
 	go func() {
 		fmt.Println(<-ch)
 		wg.Done()
 	}()
-	wg.Wait()
+	wg.Wait()*/
+	ch1, ch2 := make(chan string), make(chan string)
+	go func() {
+		ch1 <- "message to channel 1"
+	}()
+	go func() {
+		ch2 <- "message to channel 2"
+	}()
+	time.Sleep(10 * time.Millisecond)
+	select {
+	case msg := <-ch1:
+		fmt.Println(msg)
+	case msg := <-ch2:
+		fmt.Println(msg)
+	default:
+		fmt.Println("no messages available")
+	}
 }
 
 /*func divide(a, b int) error {
